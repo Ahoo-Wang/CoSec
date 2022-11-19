af9e187f4185ca60696ae4a29bca7a9b2686c5c7
Ahoo Wang <ahoowang@qq.com>
2022-11-19 18:33:16 +0800

    fix package deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -165,8 +165,8 @@ configure(publishProjects) {
                 name = "GitHubPackages"
                 url = uri("https://maven.pkg.github.com/Ahoo-Wang/CoSec")
                 credentials {
-                    username = project.findProperty("gitHubPackagesUserName") as? String
-                    password = project.findProperty("gitHubPackagesToken") as? String?
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
                 }
             }
         }
@@ -209,6 +209,14 @@ configure(publishProjects) {
         }
     }
     configure<SigningExtension> {
+        val isInCI = null != System.getenv("CI");
+        if (isInCI) {
+            val signingKeyId = System.getenv("SIGNING_KEYID")
+            val signingKey = System.getenv("SIGNING_SECRETKEY")
+            val signingPassword = System.getenv("SIGNING_PASSWORD")
+            useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+        }
+
         if (isBom) {
             sign(extensions.getByType(PublishingExtension::class).publications.get("mavenBom"))
         } else {
@@ -219,7 +227,10 @@ configure(publishProjects) {
 
 nexusPublishing {
     repositories {
-        sonatype()
+        sonatype {
+            username.set(System.getenv("MAVEN_USERNAME"))
+            password.set(System.getenv("MAVEN_PASSWORD"))
+        }
     }
 }
 
