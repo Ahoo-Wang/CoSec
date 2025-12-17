3031f5186d90b442b2bd6594706d3e850db12419
Ahoo Wang <ahoowang@qq.com>
2025-12-17 10:14:42 +0800

    feat(webflux): add OrderedCorsWebFilter with configurable order (#686)
    - Created OrderedCorsWebFilter class that implements Ordered interface
    - Extended CorsWebFilter to support custom ordering
    - Set default order to SECURITY_FILTER_ORDER - 1
    - Added constructor parameters for CorsConfigurationSource, CorsProcessor, and order
    - Implemented getOrder() method to return configured order
    - Added unit test to verify default order configuration
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

---
+++ cosec-webflux/src/test/kotlin/me/ahoo/cosec/webflux/OrderedCorsWebFilterTest.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.ahoo.cosec.webflux
+
+import io.mockk.mockk
+import me.ahoo.cosec.webflux.ReactiveSecurityFilter.Companion.SECURITY_FILTER_ORDER
+import me.ahoo.test.asserts.assert
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class OrderedCorsWebFilterTest {
+    @Test
+    fun getOrder() {
+        OrderedCorsWebFilter(mockk()).order.assert().isEqualTo(SECURITY_FILTER_ORDER - 1)
+    }
+}
