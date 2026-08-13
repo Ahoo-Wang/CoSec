32729a82e3cacd0e49b12f91b912d3f155d4fb85
Ahoo Wang <ahoowang@qq.com>
2026-08-13 20:46:36 +0800

    fix(jwt): verify access token signature during refresh to prevent forged-claim escalation

--- cosec-jwt/src/test/kotlin/me/ahoo/cosec/jwt/JwtTokenVerifierTest.kt
+++ cosec-jwt/src/test/kotlin/me/ahoo/cosec/jwt/JwtTokenVerifierTest.kt
@@ -13,12 +13,16 @@
 
 package me.ahoo.cosec.jwt
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import me.ahoo.cosec.api.principal.CoSecPrincipal
 import me.ahoo.cosec.api.token.CompositeToken
 import me.ahoo.cosec.api.token.TokenPrincipal
 import me.ahoo.cosec.api.token.TokenTenantPrincipal
 import me.ahoo.cosec.principal.SimpleTenantPrincipal
+import me.ahoo.cosec.token.SimpleCompositeToken
 import me.ahoo.cosec.token.TokenExpiredException
+import me.ahoo.cosec.token.TokenVerificationException
 import me.ahoo.cosid.test.MockIdGenerator
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
@@ -58,5 +62,40 @@ class JwtTokenVerifierTest {
         val oldToken: CompositeToken = converter.toToken(SimpleTenantPrincipal.ANONYMOUS)
         TimeUnit.SECONDS.sleep(1)
         assertThrows(TokenExpiredException::class.java) { jwtTokenVerifier.refresh<TokenPrincipal>(oldToken) }
+    }
+
+    @Test
+    fun refreshWhenAccessTokenSignatureIsForged() {
+        val oldToken: CompositeToken = jwtTokenConverter.toToken(SimpleTenantPrincipal.ANONYMOUS)
+        val legitAccessTokenId = Jwts.decode(oldToken.accessToken).id
+        val forgedAccessToken = JWT.create()
+            .withJWTId(legitAccessTokenId)
+            .withSubject(SimpleTenantPrincipal.ANONYMOUS.id)
+            .sign(Algorithm.HMAC256("attacker-secret"))
+        val forgedToken = SimpleCompositeToken(
+            accessToken = forgedAccessToken,
+            refreshToken = oldToken.refreshToken,
+        )
+
+        assertThrows(TokenVerificationException::class.java) {
+            jwtTokenVerifier.refresh<TokenPrincipal>(forgedToken)
+        }
+    }
+
+    @Test
+    fun refreshWhenAccessTokenExpiredButSignatureValid() {
+        val converter = JwtTokenConverter(
+            MockIdGenerator.INSTANCE,
+            JwtFixture.ALGORITHM,
+            Duration.ofMillis(1),
+            Duration.ofDays(7),
+        )
+        val oldToken: CompositeToken = converter.toToken(SimpleTenantPrincipal.ANONYMOUS)
+        TimeUnit.SECONDS.sleep(1)
+
+        val newTokenPrincipal = jwtTokenVerifier.refresh<TokenTenantPrincipal>(oldToken)
+
+        assertThat(newTokenPrincipal.id, equalTo(SimpleTenantPrincipal.ANONYMOUS.id))
+        assertThat(newTokenPrincipal.tenant.tenantId, equalTo(SimpleTenantPrincipal.ANONYMOUS.tenant.tenantId))
     }
 }
